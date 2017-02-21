9fc25b8848d8382a2721eeb9ad8f0089d3d9ddb7
paxmaniac <jonpaxman@gmail.com>
2017-02-21 15:41:01 +0800

    Update 1a.c

--- code-examples/1a.c
+++ code-examples/1a.c
@@ -5,19 +5,18 @@
 
 int main(void)
 {
-	//variable declarations
-	static uint8_t leds = 0;
+	// initialisation code goes in this section
+	DDRA = 0xFF;	//put PORTA into output mode
+  	PORTA = 0; 	// what does this line do?
 	
-	//initialisation section, runs once
-	DDRA = 0xFF; //configure PORTA as output
-	leds = 0;
-    PORTA = leds;
-    
-	//main loop
-	while(1)
-	{
-		leds = leds + 1; //increment the value of the leds variable by one
-        PORTA = leds; //write the value in the leds variable to PORTA, changing the state of the LEDs
-        _delay_ms(500); //500ms delay, make sure F_CPU is set
-	}
-}//end main
+	
+  	while(1)//main loop, any repeated instructions go here
+  	{
+    		_delay_ms(500);     //500 millisecond delay
+    		PORTA |= (1<<PA3);  // note here PA3 is just an alias for the number 3
+                        // this line is equivalent to PORTA = PORTA | 0b00001000   which writes a HIGH to pin 3 of PORTA
+    		_delay_ms(500); 
+    		PORTA &= ~(1<<PA3); // this line is equivalent to PORTA = PORTA & (0b11110111)  which writes a HIGH to pin 3 of PORTA
+  	}
+  	return(1);
+}//end main 

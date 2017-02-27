ae07392b80444a267456a44c7cd1740aac72c410
paxmaniac <jonpaxman@gmail.com>
2017-02-27 13:02:36 +0800

    Update 1a.c

--- code-examples/1a.c
+++ code-examples/1a.c
@@ -16,7 +16,7 @@ int main(void)
     		PORTA |= (1<<PA3);  // note here PA3 is just an alias for the number 3
                         // this line is equivalent to PORTA = PORTA | 0b00001000   which writes a HIGH to pin 3 of PORTA
     		_delay_ms(500); 
-    		PORTA &= ~(1<<PA3); // this line is equivalent to PORTA = PORTA & (0b11110111)  which writes a HIGH to pin 3 of PORTA
+    		PORTA &= ~(1<<PA3); // this line is equivalent to PORTA = PORTA & (0b11110111)  which writes a LOW to pin 3 of PORTA
   	}
   	return(1);
 }//end main 

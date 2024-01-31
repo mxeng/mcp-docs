490e3caf7f7e9695a2d3d94aa070dfbd02a027d1
MrTristanDavies <124324118+MrTristanDavies@users.noreply.github.com>
2024-01-31 15:24:19 +0800

    Update LCD_example.c

--- tutorials/LCD_example.c
+++ tutorials/LCD_example.c
@@ -18,25 +18,30 @@ int main(void)
 	lcd_init(); //initialise 
 
 	_delay_ms(20);
-	uint8_t variableToPrint;
+	uint16_t variableToPrint;
 	
 	//main loop
 	while(1)
 	{	
-		lcd_home(); // same as lcd_goto(0);
+		
+    lcd_goto(0);      //Put cursor at position 0
+    lcd_home();       // same as lcd_goto(0);
 		lcd_puts( "This is the number 32" ); //Print string to LCD first line
-		lcd_goto( 0x40 );
-		//%u for unsigned integers, %i,%d for signed integers
+		lcd_goto( 0x40 );     //Put cursor to first character on second line
 		variableToPrint = 32;
-		sprintf( lcd_string , "This is the number %3u" , variableToPrint ); // will print ### Other text
-		lcd_puts( lcd_string ); //Print string to LCD second line
-		lcd_clrscr(); //Clear everything on LCD
+		sprintf( lcd_string , "This is the number %u" , variableToPrint ); 
+    //print to string, %u special character to be replaced by variables in later arguments
+    lcd_puts( lcd_string ); //Print string to LCD second line, same as first line
+    //%u for unsigned integers, %i,%d for signed integers
+    //%lu for long unsigned ...
+    lcd_clrscr();     //Clear everything on LCD
 		variableToPrint = 111;
 		sprintf( lcd_string , "%u" , variableToPrint );
-		lcd_puts( lcd_string );
+		lcd_goto(0);     
+    lcd_puts( lcd_string ); //Screen shows 111
 		variableToPrint = 3;
 		sprintf( lcd_string , "%u" , variableToPrint );
-		lcd_puts( lcd_string ); // screen now has 311
+		lcd_puts( lcd_string ); // screen shows 311
 		sprintf( lcd_string , "%3u" , variableToPrint );
 		lcd_puts( lcd_string ); // screen now has 3
 		//loop code goes here		
